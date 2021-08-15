07a137bc1725a20183c5558d58dffd3e09d5a3f7
Mike Repec <mikerepec@gmail.com>
2021-08-15 23:50:57 +0200

    Setup simple user profile and persist user info from firebase

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,7 +1,22 @@
 <template>
   <v-row justify="center" align="center">
     <v-col>
-      <h1 class="mt-4">Hi, {{ userDisplayName }}</h1>
+      <h1 class="mt-5">Your Profile</h1>
+
+      <v-card class="mt-5" max-width="600">
+        <div class="user-name-wrapper">
+          <v-avatar v-if="userAvatar">
+            <img :src="userAvatar" :alt="userDisplayName" />
+          </v-avatar>
+
+          <div>
+            <v-card-title>
+              <span>{{ userDisplayName }}</span>
+            </v-card-title>
+            <v-card-subtitle>{{ userEmail }}</v-card-subtitle>
+          </div>
+        </div>
+      </v-card>
     </v-col>
   </v-row>
 </template>
@@ -15,11 +30,27 @@ export default Vue.extend({
   },
 
   computed: {
+    userAvatar() {
+      return this.$accessor.auth.user?.photoURL
+    },
+
     userDisplayName() {
-      return this.$accessor.auth.userName
+      const { user } = this.$accessor.auth
+
+      return user?.displayName || user?.email
+    },
+
+    userEmail() {
+      return this.$accessor.auth.user?.email
     },
   },
 })
 </script>
 
-
+<style lang="scss" scoped>
+.user-name-wrapper {
+  display: flex;
+  align-items: center;
+  padding: 0 1rem;
+}
+</style>
--- store/auth.ts
+++ store/auth.ts
@@ -3,43 +3,53 @@ import { ActionTree, MutationTree } from 'vuex/types'
 
 import { RootState } from '.'
 
+import { User } from '~/models'
+
 interface State {
   isLoggedIn: boolean
-  userName: string | null
-  user?: any
+  user: User | null
 }
 export const state = (): State => ({
   isLoggedIn: false,
-  userName: null,
+  user: null,
 })
 
 export type AuthState = ReturnType<typeof state>
 
 export const mutations: MutationTree<AuthState> = {
   setLoggedIn: (state) => (state.isLoggedIn = true),
+
   setLoggedOut: (state) => (state.isLoggedIn = false),
-  setUserName: (state, name: string) => (state.userName = name),
+
+  setUser: (state, user: firebase.UserInfo) => {
+    const { displayName, email, uid, photoURL } = user
+    state.user = { displayName, email, uid, photoURL }
+  },
+
+  clearUser: (state) => {
+    state.user = null
+  },
 }
 
 export const actions: ActionTree<AuthState, RootState> = {
   logOut({ commit }) {
     commit('setLoggedOut')
-    commit('setUserName', null)
+    commit('clearUser')
     this.$router.push({ name: 'login' })
   },
 
-  logIn({ commit }, user: firebase.User) {
-    commit('setUserName', user.displayName || user.email)
+  logIn({ commit }, user: firebase.UserInfo) {
+    commit('setUser', user)
     commit('setLoggedIn')
     this.$router.push({ name: 'profile' })
   },
 
-  onAuthStateChanged({ dispatch, state }, user: { authUser: firebase.User }) {
-    console.debug('onAuthStateChangedAction: ', user) // eslint-disable-line
+  onAuthStateChanged({ dispatch, state }, payload: { authUser: firebase.User }) {
+    console.debug('onAuthStateChangedAction: ', payload) // eslint-disable-line
     const { isLoggedIn } = state
 
-    if (user.authUser) {
-      dispatch('logIn', user.authUser)
+    if (payload.authUser) {
+      dispatch('logIn', payload.authUser)
     } else if (isLoggedIn) {
       dispatch('logOut')
     }
